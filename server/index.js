c2e4d3843e92aeb0d234018066326e8364ec756e
Ian Richard <ian.richard@webfurnace.com>
2025-08-02 23:09:36 -0500

    Setup get api/detachment-types

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-
+const mongoose = require('mongoose');   //DB ORM
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const detachmentRoutes = require('./routes/detachmentRoutes');
+const DetachmentTypeRoutes = require('./routes/detachment-types');
 
 //Load Config and Secrets off Server
 require('dotenv').config();
 
 //ORM and Database
-const mongoose = require('mongoose');   //DB ORM
+
 
 //DB Connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TraitorBuddy', {
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // API routes (placeholder)
 app.get('/api/ping', (req, res) => res.json({ message: 'pong' }));
 
-app.use('/api/detachments', detachmentRoutes);
+app.use('/api/detachment-types', DetachmentTypeRoutes);
 
 // Catch-all to serve React
 app.get('/{*splat}', (req, res) => {
